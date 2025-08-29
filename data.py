d6b8289e5dd12eb3fb9530d833cd02675506d8e6
Celest Ward <celest.rios@outlook.com>
2025-08-29 17:08:37 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,17 +1,15 @@
-# data.py
-
-# Addresses for the tests
-FROM_ADDRESS = "123 Main St"
-TO_ADDRESS = "456 Elm St"
-
-# Phone number used for phone confirmation test
-PHONE_NUMBER = "+1234567890"
-
-# Comment/message for the driver
-DRIVER_COMMENT = "Please be careful, I have a lot of luggage."
-
-# Credit card info (use valid test card numbers only)
-CREDIT_CARD_NUMBER = "4111111111111111"
-CREDIT_CARD_CODE = "123"
-
-# Additional test data can be added here as needed
+URBAN_ROUTES_URL = "https://cnt-32008ade-6882-402c-af93-f87fa0ba1a28.containerhub.tripleten-services.com"
+ 
+ 
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+ 
+#phone number for testing
+PHONE_NUMBER = '+1 123 123 12 12'
+ 
+#credit card information
+CARD_NUMBER = '1234 0000 5678'
+CARD_CODE = '1234'
+ 
+MESSAGE_FOR_DRIVER = "Please pick me up at the corner, thanks!"
+ 
